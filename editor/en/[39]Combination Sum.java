9aec9fd72a3e6c26ce1f7e19a2726b7c366f569e
Kaidong Shen <kaidong.shen@foxmail.com>
2022-02-19 11:21:20 -0500

    Finished [39]Combination Sum

---
+++ editor/en/[39]Combination Sum.java
@@ -0,0 +1,75 @@
+//Given an array of distinct integers candidates and a target integer target, re
+//turn a list of all unique combinations of candidates where the chosen numbers su
+//m to target. You may return the combinations in any order. 
+//
+// The same number may be chosen from candidates an unlimited number of times. T
+//wo combinations are unique if the frequency of at least one of the chosen number
+//s is different. 
+//
+// It is guaranteed that the number of unique combinations that sum up to target
+// is less than 150 combinations for the given input. 
+//
+// 
+// Example 1: 
+//
+// 
+//Input: candidates = [2,3,6,7], target = 7
+//Output: [[2,2,3],[7]]
+//Explanation:
+//2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple ti
+//mes.
+//7 is a candidate, and 7 = 7.
+//These are the only two combinations.
+// 
+//
+// Example 2: 
+//
+// 
+//Input: candidates = [2,3,5], target = 8
+//Output: [[2,2,2,2],[2,3,3],[3,5]]
+// 
+//
+// Example 3: 
+//
+// 
+//Input: candidates = [2], target = 1
+//Output: []
+// 
+//
+// 
+// Constraints: 
+//
+// 
+// 1 <= candidates.length <= 30 
+// 1 <= candidates[i] <= 200 
+// All elements of candidates are distinct. 
+// 1 <= target <= 500 
+// 
+// Related Topics Array Backtracking 
+// 👍 8789 👎 206
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        helper(candidates, target, 0, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void helper(int[] candidates, int target, int start, int sum, List<Integer> list, List<List<Integer>> res) {
+        if (sum > target) {
+            return;
+        }
+        if (sum == target) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            list.add(candidates[i]);
+            helper(candidates, target, i , sum + candidates[i], list, res);
+            list.remove(list.size() - 1);
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
