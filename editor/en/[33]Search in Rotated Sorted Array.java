7e4f6efab9d208c3f3ab336129e280a67b8805b8
Kaidong Shen <kaidong.shen@foxmail.com>
2022-03-16 18:05:15 -0400

    Finished [33]Search in Rotated Sorted Array

--- editor/en/[33]Search in Rotated Sorted Array.java
+++ editor/en/[33]Search in Rotated Sorted Array.java
@@ -54,47 +54,48 @@ class Solution {
         if (nums == null || nums.length == 0) {
             return -1;
         }
-        int left = 0;
-        int right = nums.length - 1;
-        while (left + 1 < right) {
-            int mid = left + (right - left) / 2;
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo + 1 < hi) {
+            int mid = lo + (hi - lo) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
-            // 从left到mid是递增的
-            if (nums[mid] >= nums[left]) {
-                // target>= left, target in the right side of left, or it is "left"
-                // target < nums[mid] which means target will find between the left to mid
-                if (target >= nums[left] && target < nums[mid]) {
-                    // so we move right to mid, to shrink the size of the array
-                    right = mid;
-                    // if the target is not between the left to mid, we consider moving the left to mid
+            // 从lo到mid是递增的
+            if (nums[mid] >= nums[lo]) {
+                // target>= lo, target in the hi side of lo, or it is "lo"
+                // target < nums[mid] which means target will find between the lo to mid
+                if (target >= nums[lo] && target < nums[mid]) {
+                    // so we move hi to mid, to shrink the size of the array
+                    hi = mid;
+                    // if the target is not between the lo to mid, we consider moving the lo to mid
                 } else {
-                    left = mid;
+                    lo = mid;
                 }
-                // if left to mid not ascending order, we start at the right of mid
+                // if lo to mid not ascending order, we start at the hi of mid
             } else {
-                // target <= nums[right], target is in the left side of this subarray, or it is "right"
-                // target > nums[mid], which target will find between the mid to right
-                if (target <= nums[right] && target > nums[mid]) {
-                    // so we move left to mid
-                    left = mid;
-                    // if the target is not between the mid to the right, that we move right to mid
+                // target <= nums[hi], target is in the lo side of this subarray, or it is "hi"
+                // target > nums[mid], which target will find between the mid to hi
+                if (target <= nums[hi] && target > nums[mid]) {
+                    // so we move lo to mid
+                    lo = mid;
+                    // if the target is not between the mid to the hi, that we move hi to mid
                 } else {
-                    right = mid;
+                    hi = mid;
                 }
             }
         }
-        if (nums[left] == target) {
-            return left;
+        if (nums[lo] == target) {
+            return lo;
         }
-        return nums[right] == target ? right : -1;
+        return nums[hi] == target ? hi : -1;
     }
 }
 
+
 // This question first finds consecutive sub-arrays, and then first sees whether the target is in the sub-array, if so, the result is in this array
-// If not, move one of the left and right sides, adjust the size of the array, and then search again,
+// If not, move one of the lo and hi sides, adjust the size of the array, and then search again,
 // Go back to the beginning, first find the continuously ascending sub-array, and then confirm the position of the target in the continuous sub-array
 // The difficulty lies in how to deal with the rotated situation, in fact, there is one more judgment,
-// If the sub-array itself is ascending, it is consistent with the ordinary binary search, if not, you need to move the left and right sides, narrow the range and continue
+// If the sub-array itself is ascending, it is consistent with the ordinary binary search, if not, you need to move the lo and hi sides, narrow the range and continue
 //leetcode submit region end(Prohibit modification and deletion)
